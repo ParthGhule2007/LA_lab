3037f1ac3795cfe4985fb685b68983ed453d4ce3
ParthGhule2007 <parthghule2007@gmail.com>
2026-08-25 17:40:01 +0530

    Refactor file processing from vim to Python script
    Replaced vim commands with a Python script to process file contents. The script cleans up the output by removing specific lines and formatting the remaining data.

--- Fifth.sh
+++ Fifth.sh
@@ -8,13 +8,32 @@ lab_file="editing_final_lab.txt"
 
 ls -la ~ > "$lab_file"
 
-vim -es -c '1,3d' \
-        -c '%s/^\(....\)[^ ]*\( \+[0-9]\+ \+[^ ]\+\) \+[^ ]\+/\1\2/' \
-        -c '%s/ \+[0-9]\{1,2\}:[0-9]\{2\}//' \
-        -c '%s/ \+[0-9]\{4\}//' \
-        -c 'g/Desktop/d' \
-        -c 'g/Public/d' \
-        -c 'wq' "$lab_file"
+python3 -c '
+import re
+
+with open("editing_final_lab.txt", "r") as f:
+    lines = f.readlines()[3:]  # remove first 3 lines
+
+cleaned = []
+for line in lines:
+    if "Desktop" in line or "Public" in line:
+        continue
+    # Line format: perms links user group size month day time/year name
+    # Trim perms to 4 chars, remove group (col 4), remove time/year (col 8)
+    parts = line.split(None, 8)
+    if len(parts) >= 9:
+        perms = parts[0][:4]
+        links = parts[1]
+        user = parts[2]
+        size = parts[4]
+        month = parts[5]
+        day = parts[6]
+        name = parts[8]
+        cleaned.append(f"{perms} {links} {user} {size} {month} {day} {name}")
+
+with open("editing_final_lab.txt", "w") as f:
+    f.writelines(cleaned)
+'
 
 cp "$lab_file" "${lab_file}_$(date +%s)"
 
